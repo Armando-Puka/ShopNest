52f60a7eb070ba4bbfafb71348459ba1d561ab13
Armando-Puka <armadopouka@gmail.com>
2024-10-15 15:55:10 +0200

    ShopNest v1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './components/header/header.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ShopNest';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AdminCategoryComponent } from './components/admin-category/admin-category.component';
+import { AdminAuthGuard, AuthGuard } from './guards/auth.guard';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+// import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+    { path: 'signup', component: SignupComponent, canActivate: [AuthGuard] },
+    { path: 'admin/categories', component: AdminCategoryComponent, canActivate: [AdminAuthGuard], data: { roles: ['admin'] }},
+    { path: 'admin/products', component: AddProductComponent, canActivate: [AdminAuthGuard], data: { roles: ['admin'] }},
+    { path: 'product/:id', component: ProductDetailsComponent},
+    // { path: 'cart', component: },
+    { path: '**', redirectTo: '' }
+    // { path: 'admin-access', component: AdminAccessComponent},
+    // { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
